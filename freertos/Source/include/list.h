5fefee2a13a2bd5d9a8b48b22d6afca9030a58e6
4infantasy <4infantasy@gmail.com>
2020-01-07 18:29:17 +0800

    modified:   Project/Listings/My_FreeRTOS.map 	modified:   Project/My_FreeRTOS.uvguix.Jnu 	modified:   Project/Objects/My_FreeRTOS.build_log.htm 	modified:   Project/Objects/My_FreeRTOS.htm 	modified:   Project/Objects/My_FreeRTOS_Target 1.dep 	modified:   Project/Objects/list.crf 	modified:   Project/Objects/list.o 	modified:   freertos/Source/include/list.h 	modified:   freertos/list.c

--- freertos/Source/include/list.h
+++ freertos/Source/include/list.h
@@ -38,5 +38,49 @@ typedef struct xLIST
 } List_t;
 
 
+// 初始化节点的拥有者
+#define	listSET_LIST_ITEM_OWNER( pxListItem, pxOwner ) \
+		( ( pxListItem )->pvOwner = ( void * ) (pxOwner) )
+				
+// 获取节点拥有者
+#define	listGET_LIST_ITEM_OWNER( pxListItem ) \
+		( ( pxListItem )->pvOwner )
+		
+// 初始化节点排序辅助值
+#define	listSET_LIST_ITEM_VALUE( pxListItem, xValue ) \
+		( ( pxListItem )->xItemValue = ( xValue ) )
+		
+// 获取节点排序辅助值
+#define	listGET_LIST_ITEM_VALUE( pxListItem ) \
+		( ( pxListItem )->xItemValue )
+		
+// 获取链表根节点的节点计数器的值
+#define	listGET_ITEM_VALUE_OF_HEAD_ENTRY( pxList ) \
+		( ( ( pxList )->xListEnd ).pxNext->xItemValue )
+		
+// 获取链表的入口节点
+#define	listGET_HEAD_ENTRY( pxList ) \
+		( ( ( pxList )->xListEnd ).pxNext )
+		
+// 获取节点的下一个节点
+#define	listGET_NEXT( pxListItem ) \
+		( ( pxListItem )->pxNext )
+		
+// 获取链表的最后一个节点
+#define	listGET_END_MARKER( pxList ) \
+		( ( ListItem_t const * ) ( &( ( pxList )->xListEnd ) ) )
+			
+// 判断链表是否为空
+#define	listLIST_IS_EMPTY( pxList ) \
+		( ( BaseType_t ) ( ( pxList )->uxNumberOfItems == ( UBaseType_t ) 0 ) )
+
+// 获取链表的节点数
+#define	listCURRENT_LIST_LENGTH( pxList ) \
+		( ( pxList )->uxNumberOfItems )
+		
+// 获取链表第一个节点的OWNER，即TCB
+#define	listGET_OWNER_OF_NEXT_ENTRY( pxTCB, pxList ) 	\
+{	\
+}	\
 
 
--- freertos/list.c
+++ freertos/list.c
@@ -53,6 +53,71 @@ void vListInsertEnd( List_t * const pxList, ListItem_t * const pxNewListItem )
 }
 
 
+// 将字节按照升序排列插入到链表
+void vListInsert( List_t * const pxList, ListItem_t * const pxNewListItem )
+{
+
+	ListItem_t *pxIterator;
+	
+	// 获取节点的排序辅助值
+	const TickType_t xValueOfInsertion = pxNewListItem->xItemValue;
+	
+	// 寻找节点要插入的位置
+	if ( xValueOfInsertion == portMAX_DELAY ) 
+	{
+		pxIterator = pxList->xListEnd.pxPrevious;
+	}
+	else
+	{
+		for ( pxIterator = ( ListItem_t * ) &( pxList->xListEnd );
+				pxIterator->pxNext->xItemValue <= xValueOfInsertion;
+				pxIterator = pxIterator->pxNext )
+		{
+			// 没有事情可做，不断迭代只是为了找到节点要插入的位置
+		}
+	}
+	
+	// 根据升序排列，将节点插入
+	pxNewListItem->pxNext = pxIterator->pxNext;
+	pxNewListItem->pxNext->pxPrevious = pxNewListItem;
+	
+	pxNewListItem->pxPrevious = pxIterator;
+	pxIterator->pxNext = pxNewListItem;
+	
+	// 记住该节点所在的链表
+	pxNewListItem->pvContainer = ( void * ) pxList;
+	
+	// 链表节点计数器++
+	(pxList->uxNumberOfItems)++ ;
+
+}
 
 
+// 将节点从链表删除
+UBaseType_t uxListRemove( ListItem_t * const pxItemToRemove )
+{
+
+	// 获取节点所在链表
+	List_t * const pxList = ( List_t * ) pxItemToRemove->pvContainer;
+	
+	// 将指定的节点从链表删除
+	pxItemToRemove->pxNext->pxPrevious = pxItemToRemove->pxPrevious;
+	pxItemToRemove->pxPrevious->pxNext = pxItemToRemove->pxNext;
+	
+	// 调整链表的节点索引指针
+	if ( pxList->pxIndex == pxItemToRemove ) 
+	{
+		pxList->pxIndex = pxItemToRemove->pxPrevious;
+	}
+	
+	// 初始化该节点所在的链表为空，表示该节点还没有插入任何链表
+	pxItemToRemove->pvContainer = NULL;
+	
+	// 链表节点计数器--
+	( pxList->uxNumberOfItems )--;
+	
+	// 返回链表中剩余节点的个数
+	return pxList->uxNumberOfItems;
+	
+}
 
